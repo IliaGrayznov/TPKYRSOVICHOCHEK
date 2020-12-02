ab618dc77b81fc1bad007a0a8080a074fe6cfa8a
IliaGrayznov <64246025+IliaGrayznov@users.noreply.github.com>
2020-12-02 11:53:08 +0300

    Now all new users are with role Buyers

--- WebApplication2/WebApplication1/Controllers/AccountController.cs
+++ WebApplication2/WebApplication1/Controllers/AccountController.cs
@@ -152,11 +152,11 @@ namespace WebApplication1.Controllers
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
+                var result = await UserManager.CreateAsync(user, model.Password);               
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                    await UserManager.AddToRoleAsync(user.Id, "Buyer");
                     // Дополнительные сведения о включении подтверждения учетной записи и сброса пароля см. на странице https://go.microsoft.com/fwlink/?LinkID=320771.
                     // Отправка сообщения электронной почты с этой ссылкой
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
